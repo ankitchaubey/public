20ada49ece6055576c98705e4bec79dbbaaf907d
ankitchaubey <45657497+ankitchaubey@users.noreply.github.com>
2018-12-07 23:37:37 +0530

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-web_hook_url= "https://hooks.slack.com/services/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"
+web_hook_url= "path"
 
 slack_msg = {"text": "hello from python" "text": "pageralert duity"}
 requests.post(web_hook_url,data=json.dumps(slack_msg))
