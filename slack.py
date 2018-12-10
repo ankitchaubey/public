168087eb49ae8285113f85c4005219049e1f7b47
ankitchaubey <ankitchaubey009@gmail.com>
2018-12-10 13:31:38 +0530

    git

--- slack.py
+++ slack.py
@@ -1,7 +1,16 @@
 import requests
 import json
 
-web_hook_url= "https://hooks.slack.com/services/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"
+web_hook_url = "https://hooks.slack.com/services/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"
 
-slack_msg = {"text": "hello from python" "text": "pageralert duity"}
-requests.post(web_hook_url,data=json.dumps(slack_msg))
+image_url = "http://cdn.cloudinary.com/demo/image/upload/w_400/hungry_cat.jpg"
+slack_msg = {"username": "ankit",
+             "text": "When it is time for lunch, please remember not to delay and go ahead to have lunch. Other wise you will miss the salad, Dessert",
+             "attachments": [
+                 {
+                     "color": "good",
+                     "image_url": "http://funkyimg.com/i/2P2CM.png"
+                 }
+             ]
+             }
+requests.post(web_hook_url, data=json.dumps(slack_msg), headers={'Content-Type': 'application/json'})
